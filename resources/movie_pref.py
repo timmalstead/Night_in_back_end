cd0b309afb343ebcfeb7b66b1b4c0275ff09a39f
Carlos <demirose41@gmail.com'git commitgit logpwdNsdad>
2019-11-21 09:27:32 -0800

    dummy

--- resources/movie_pref.py
+++ resources/movie_pref.py
@@ -1,27 +1,27 @@
-# UNDER CONSTRUCTION 
+UNDER CONSTRUCTION 
 
-# import models 
+import models 
 
-# from flask import request, jsonify, Blueprint 
+from flask import request, jsonify, Blueprint 
 
-# from playhouse.shortcuts import model_to_dict
+from playhouse.shortcuts import model_to_dict
 
-# movie_pref = Blueprint('movie_prefs', 'movie_pref')
+movie_pref = Blueprint('movie_prefs', 'movie_pref')
 
-# @movie_pref.route('/',methods=["POST"])
-# def register():
-#     payload = request.get_json()
+@movie_pref.route('/',methods=["POST"])
+def register():
+    payload = request.get_json()
 
-#     try:
-#         models.movie_pref.get(models.movie_pref.user === payload ['user_id'])
-#         return jsonify(data={}, status={"code":401, "message":"A user with that email already exists"})
-#     except models.DoesNotExist:
-#         movie_pref = models.movie_pref.create(**payload)
+    try:
+        models.movie_pref.get(models.movie_pref.user === payload ['user_id'])
+        return jsonify(data={}, status={"code":401, "message":"A user with that email already exists"})
+    except models.DoesNotExist:
+        movie_pref = models.movie_pref.create(**payload)
 
-#         movie_pref_dict = model_to_dict(list)
-#         return jsonify(data = list_dict, status={"code": 201, "message": "Success"})
+        movie_pref_dict = model_to_dict(list)
+        return jsonify(data = list_dict, status={"code": 201, "message": "Success"})
         
-# @movie_pref.route('/',methods=["GET"])
-# def get_movie_pref():
-#     try:
-#         movie_prefs = [model_to_dict(movie_pref) for movie_pref in modles.movie_pref.select().where(movie_pref.user == )
+@movie_pref.route('/',methods=["GET"])
+def get_movie_pref():
+    try:
+        movie_prefs = [model_to_dict(movie_pref) for movie_pref in modles.movie_pref.select().where(movie_pref.user == )
